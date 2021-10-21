9220a65d48bf91f89a596ba46d4634e22c093369
Nazar Hetun <nazarhetun@gmail.com>
2021-10-21 12:41:52 +0300

    heroku files

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://remotemysql.com:3306/taxi?serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://remotemysql.com:3306/0KTQfJxVIW";
     private static final String USERNAME = "0KTQfJxVIW";
     private static final String PASSWORD = "krjtEmrmsJ";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
